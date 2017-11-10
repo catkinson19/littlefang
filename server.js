2f5ec4c3a551c25e0b037e6e8ad037ba126e90e2
cjatkinson <cjatkinson@homeadvisor.com>
2017-11-10 13:50:51 -0700

    port issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const formScraper = require("form-scraper");
 
 const app = express();
-const PORT = 9999;
+var PORT = process.env.PORT || 9999;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
